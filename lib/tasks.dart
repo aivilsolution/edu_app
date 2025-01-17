f5b5097cfd386dd6f9998c219168bc778e49caa6
atheeq-rhxn <atheeq.rhxn@gmail.com>
2025-01-17 18:27:30 +0530

    fixing overflow for liveview.builder

--- lib/tasks.dart
+++ lib/tasks.dart
@@ -5,89 +5,90 @@ class Tasks extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Tasks',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
+    return Flexible(
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Tasks',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-                Row(
-                  children: [
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.timer_outlined,
-                        size: 28,
-                        color: Colors.white70,
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.timer_outlined,
+                          size: 28,
+                          color: Colors.white70,
+                        ),
+                        padding: const EdgeInsets.all(12),
                       ),
-                      padding: const EdgeInsets.all(12),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.add,
-                        size: 28,
-                        color: Colors.white70,
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.add,
+                          size: 28,
+                          color: Colors.white70,
+                        ),
+                        padding: const EdgeInsets.all(12),
                       ),
-                      padding: const EdgeInsets.all(12),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Container(
-              height: 200,
-              decoration: BoxDecoration(),
-              child: ListView.separated(
-                itemCount: 5,
-                separatorBuilder: (context, index) => const Divider(height: 1),
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    contentPadding: const EdgeInsets.symmetric(
-                      vertical: 8,
-                    ),
-                    leading: Transform.scale(
-                      scale: 1.1,
-                      child: Checkbox(
-                        value: false,
-                        onChanged: (bool? value) {},
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(4),
+                    ],
+                  ),
+                ],
+              ),
+              Flexible(
+                child: ListView.separated(
+                  itemCount: 10,
+                  separatorBuilder: (context, index) =>
+                      const Divider(height: 1),
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      contentPadding: const EdgeInsets.symmetric(
+                        vertical: 8,
+                      ),
+                      leading: Transform.scale(
+                        scale: 1.1,
+                        child: Checkbox(
+                          value: false,
+                          onChanged: (bool? value) {},
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4),
+                          ),
                         ),
                       ),
-                    ),
-                    title: Text(
-                      'Task ${index + 1}',
-                      style: const TextStyle(
-                        fontSize: 20,
+                      title: Text(
+                        'Task ${index + 1}',
+                        style: const TextStyle(
+                          fontSize: 20,
+                        ),
                       ),
-                    ),
-                    trailing: IconButton(
-                      icon: const Icon(
-                        Icons.delete_outline,
-                        size: 24,
-                        color: Colors.grey,
+                      trailing: IconButton(
+                        icon: const Icon(
+                          Icons.delete_outline,
+                          size: 24,
+                          color: Colors.grey,
+                        ),
+                        onPressed: () {},
                       ),
-                      onPressed: () {},
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
